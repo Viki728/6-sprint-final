1f2a2c5654d6636b5d30e471a487bdc6d74f34c7
Viktoriy Malukova <malukova-99@ya.ru>
2025-09-02 13:25:51 +0300

    Добавлены пакеты handlers и server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,41 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+// Создаем структуру сервера
+type Server struct {
+	Logger *log.Logger
+	HTTP   *http.Server
+}
+
+// Функция создания сервера
+func NewServer(logger *log.Logger) *Server {
+	//Роутер
+	mux := http.NewServeMux()
+
+	//Регистрируем хендлеры
+	mux.HandleFunc("/", handlers.HandlerIndex)
+	mux.HandleFunc("/upload", handlers.HandlerUpload)
+
+	//Создаем экземпляр стуктуры http.Server
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	//Возвращаем ссылку на сервер
+	return &Server{
+		Logger: logger,
+		HTTP:   srv,
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+// Conv определяет формат и возвращает конвертированный результат
+func Conv(input string) (string, error) {
+	//убираем лишние пробелы вначале и в конце строки
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		return "", errors.New("пустая строка")
+	}
+
+	//Конвертируем строку
+	//если только точки, тире и пробелы
+	if isMorse(input) {
+
+		//переводим морзе в текст
+		return morse.ToText(input), nil
+	}
+
+	//иначе переводим текст в морзе
+	return morse.ToMorse(input), nil
+}
+
+// Проверяем морзе ли это
+func isMorse(s string) bool {
+	for _, r := range s {
+		//возврвщвем false в том случае если символ НЕ точка, НЕ тире, и НЕ пробел
+		if r != '.' && r != '-' && r != ' ' {
+			return false
+		}
+	}
+	return true
+}
